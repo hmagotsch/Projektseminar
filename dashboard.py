705b274a5e9bd44bd9348891b8fd51a05ab7c3ef
Tim Lickert <119865838+timlickert@users.noreply.github.com>
2024-03-18 16:55:03 +0100

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -35,7 +35,427 @@ def home():
 
 def page1():
     st.title("KPI")
+
+    # Upload data
+    uploaded_cluster_file_KPI = st.file_uploader('Upload your data (CSV file)', type='csv')
     # Hier Inhalt für Seite 2 definieren
+
+    if uploaded_cluster_file_KPI is not None:
+      #Maschine A - KPI
+      import numpy as np
+      import statsmodels.api as sm
+      from scipy.stats import t
+
+      # Read the CSV file into a pandas DataFrame
+      df = pd.read_csv(uploaded_cluster_file_KPI, sep=';')
+
+      #gewichtete kombinierte Zeit pro Sheet
+
+      # Read the CSV file into a pandas DataFrame
+
+      # Convert the "CheckIn" and "CheckOut" columns to datetime objects with the specified format
+      date_format = "%Y-%m-%d %H:%M:%S.%f%z"
+      df['CheckIn'] = pd.to_datetime(df['CheckIn'], format=date_format)
+      df['CheckOut'] = pd.to_datetime(df['CheckOut'], format=date_format)
+
+      # Add an additional row where speed is not equal to 0 for the last checkout of the last job
+      last_checkout = df[df['Job'] == df['Job'].max()]['CheckOut'].max()
+      df = df.append({'Machine': df['Machine'].iloc[-1],
+                      'FileDate': df['FileDate'].iloc[-1],
+                      'CheckIn': last_checkout,
+                      'CheckOut': last_checkout,
+                      'Speed': 1,  # Change to the appropriate non-zero speed value
+                      'Gross': df['Gross'].iloc[-1],
+                      'Net': df['Net'].iloc[-1],
+                      'MsgRank': df['MsgRank'].iloc[-1],
+                      'MsgValueDE': df['MsgValueDE'].iloc[-1],
+                      'Phase': df['Phase'].iloc[-1],
+                      'Job': df['Job'].max()},
+                    ignore_index=True)
+
+
+      # Initialize dictionaries to keep track of the production time, job time, and non-productive time for each job
+      job_production_times = {}
+      job_job_times = {}
+      job_non_productive_times = {}
+      job_gained_net = {}
+
+      current_job = 0
+      production_start_time = None
+      non_productive_start_time = None
+      non_productive_time = pd.Timedelta(0)
+      gained_net_start = 0
+
+      for index, row in df.iterrows():
+          phase = row['Phase']
+          job = row['Job']
+          checkin = row['CheckIn']
+          checkout = df.at[index - 1, 'CheckOut'] if index > 0 else None
+          speed = row['Speed']
+
+          if phase == 'Production Time' and job != current_job:
+              # When 'Production Time' is encountered, store the CheckIn timestamp as the start time
+              production_start_time = checkin
+              current_job = job
+
+              # Reset non-productive time variables
+              non_productive_start_time = None
+              non_productive_time = pd.Timedelta(0)
+
+          #Phase off is not supposed to be added to production time
+          if phase == 'off':
+              while index + 1 < len(df) and df.iloc[index + 1]['Phase'] != 'Production Time':
+                  index += 1
+              if index + 1 < len(df):
+                  row = df.iloc[index + 1]
+                  phase = row['Phase']
+              else:
+                  break
+              continue
+
+
+          if speed == 0:
+              # When 'Speed' is zero, mark it as a non-productive time period
+              if non_productive_start_time is None:
+                  non_productive_start_time = checkin
+
+          else:
+              # If 'Speed' is not zero and there was a non-productive time, add it to the non-productive time for the job
+              if non_productive_start_time is not None:
+                  non_productive_time += checkout - non_productive_start_time
+                  non_productive_start_time = None
+
+          if job != current_job:
+              # Job has changed, calculate the gained net
+              gained_net_end = df.loc[(df['Job'] == job)]['Net'].iloc[-1]
+              gained_net = gained_net_end - gained_net_start
+              job_gained_net[job] = gained_net
+              gained_net_start = row['Net']
+
+          # Check if this row belongs to the same job and if it's a later timestamp
+          if job == current_job and checkin > production_start_time:
+              job_production_times[current_job] = checkin - production_start_time
+              job_non_productive_times[current_job] = non_productive_time
+
+      # Calculate the job time for each job
+      df['JobTime'] = df.groupby('Job')['CheckOut'].transform('max') - df.groupby('Job')['CheckIn'].transform('min')
+      job_job_times = df.groupby('Job')['JobTime'].first()
+
+      # Create a list to store the KPI for each job
+      kpis = []
+
+      # Print the production time, job time, non-productive time, quotient, gained net, and kpi for each job
+      for job in job_production_times.keys():
+          production_time = job_production_times.get(job, pd.Timedelta(0))
+          job_time = job_job_times.get(job, pd.Timedelta(0))
+          non_productive_time = job_non_productive_times.get(job, pd.Timedelta(0))
+          gained_net = job_gained_net.get(job, 0)
+
+          if job_time.total_seconds() == 0:
+              quotient = None
+          else:
+              unproductive_per_net = non_productive_time.total_seconds() / gained_net
+              productive_per_net = (production_time.total_seconds() - non_productive_time.total_seconds()) / gained_net
+              kpi = 4.23 * unproductive_per_net + productive_per_net
+              kpis.append(kpi)
+
+          print(f'Job {job}: Production Time = {production_time}, Non-Productive Time = {non_productive_time}, unproductive_per_net = {unproductive_per_net},productive_per_net = {productive_per_net}, Gained Net = {gained_net}, KPI = {kpi}')
+
+      # Create a line chart for KPI
+      KPI_figure_2 = plt.figure(figsize=(10, 6))
+      plt.plot(range(1, len(kpis) + 1), kpis, color='green', label='KPI', marker='o')
+      plt.xlabel('Job')
+      plt.ylabel('KPI')
+      plt.title('KPI1 for each Job - Machine A')
+      plt.xticks(range(1, len(kpis) + 1))
+      plt.grid(True)
+      plt.show()
+
+      st.pyplot(KPI_figure_2)
+
+      # Convert the "CheckIn" and "CheckOut" columns to datetime objects with the specified format
+      date_format = "%Y-%m-%d %H:%M:%S.%f%z"
+      df['CheckIn'] = pd.to_datetime(df['CheckIn'], format=date_format)
+      df['CheckOut'] = pd.to_datetime(df['CheckOut'], format=date_format)
+
+      # Add an additional row where speed is not equal to 0 for the last checkout of the last job
+      last_checkout = df[df['Job'] == df['Job'].max()]['CheckOut'].max()
+      df = df.append({'Machine': df['Machine'].iloc[-1],
+                      'FileDate': df['FileDate'].iloc[-1],
+                      'CheckIn': last_checkout,
+                      'CheckOut': last_checkout,
+                      'Speed': 1,  # Change to the appropriate non-zero speed value
+                      'Gross': df['Gross'].iloc[-1],
+                      'Net': df['Net'].iloc[-1],
+                      'MsgRank': df['MsgRank'].iloc[-1],
+                      'MsgValueDE': df['MsgValueDE'].iloc[-1],
+                      'Phase': df['Phase'].iloc[-1],
+                      'Job': df['Job'].max()},
+                  ignore_index=True)
+
+
+      # Initialize dictionaries and variables
+      job_production_times = {}
+      job_job_times = {}
+      job_non_productive_times = {}
+      job_gained_net = {}
+      job_gained_gross = {}
+      job_basic_make_ready_times = {}
+      job_fine_tune_times = {}
+      job_off_times_pre = {}
+      job_off_times_prod = {}
+      job_max_speed = {}
+      current_job = 0
+      current_phase = None
+      production_start_time = None
+      off_start_time_pre = None
+      off_start_time_prod = None
+      non_productive_start_time = None
+      non_productive_time = pd.Timedelta(0)
+      gained_net_start = 0
+      gained_gross_start = 0
+      job_fine_tune_times = {}
+      job_basic_make_ready_times = {}
+      fine_tune_start_time = None
+      basic_make_ready_start_time = None
+      fine_tune_time = pd.Timedelta(0)
+      basic_make_ready_time = pd.Timedelta(0)
+      off_time_pre = pd.Timedelta(0)
+      off_time_prod = pd.Timedelta(0)
+      new_speed_value = 0  # You can set any default value here
+
+      #Determine gained gross and net value for each job
+      for job in df['Job'].unique():
+          job_data = df[df['Job'] == job]
+
+          # Calculate gained net for the job
+          net_start = job_data['Net'].min()
+          net_end = job_data['Net'].max()
+          gained_net = net_end - net_start
+          job_gained_net[job] = gained_net
+
+          # Calculate gained gross for the job
+          gross_start = job_data['Gross'].min()
+          gross_end = job_data['Gross'].max()
+          gained_gross = gross_end - gross_start
+          job_gained_gross[job] = gained_gross
+
+          #Maximum Speed for each job
+          Max_speed = job_data['Speed'].max()
+          job_max_speed[job] = Max_speed
+
+      #Loop over whole csv file
+      for index, row in df.iterrows():
+          phase = row['Phase']
+          job = row['Job']
+          checkin = row['CheckIn']
+          checkout = df.at[index - 1, 'CheckOut'] if index > 0 else None
+          speed = row['Speed']
+
+      #Production Time
+
+          if phase == 'Production Time' and job != current_job:
+              # When 'Production Time' is encountered, store the CheckIn timestamp as the start time
+              production_start_time = checkin
+              current_job = job
+
+              # Reset non-productive time variables
+              non_productive_start_time = None
+              non_productive_time = pd.Timedelta(0)
+
+      #non-productive Time
+          if speed == 0:
+              # When 'Speed' is zero, mark it as a non-productive time period
+              if non_productive_start_time is None:
+                  non_productive_start_time = checkin
+
+          else:
+              # If 'Speed' is not zero and there was a non-productive time, add it to the non-productive time for the job
+              if non_productive_start_time is not None:
+                  non_productive_time += checkin - non_productive_start_time
+                  non_productive_start_time = None
+
+      #Basic Make Ready Time
+          if phase == 'Basic Make Ready Time':
+              if basic_make_ready_start_time is None:
+                  basic_make_ready_start_time = checkin
+                  fine_tune_time = pd.Timedelta(0)
+                  off_time_pre = pd.Timedelta(0)
+                  off_time_prod = pd.Timedelta(0)
+
+          if phase == 'Fine Tune Time' or phase == 'off':
+              if basic_make_ready_start_time is not None:
+                  basic_make_ready_time = checkin - basic_make_ready_start_time
+                  basic_make_ready_start_time = None
+      #Fine Tune Time
+          if phase == 'Fine Tune Time':
+              if fine_tune_start_time is None:
+                  fine_tune_start_time = checkin
+
+          if (phase == 'Production Time' or phase == 'off') and current_phase == 'Fine Tune Time':
+              if fine_tune_start_time is not None:
+                  fine_tune_time += checkin - fine_tune_start_time
+                  fine_tune_start_time = None
+
+      #Off Time in Preprocessing
+          if phase == 'off' and (current_phase == 'Fine Tune Time' or current_phase == 'Basic Make Ready Time') :
+              if off_start_time_pre is None:
+                  off_start_time_pre = checkin
+
+          if phase == 'On' and current_phase == 'off':
+              if off_start_time_pre is not None:
+                  off_time_pre += checkin - off_start_time_pre
+                  off_start_time_pre = None
+
+      #Off Time in Production
+          if phase == 'off' and current_phase == 'Production Time':
+              if off_start_time_prod is None:
+                  off_start_time_prod = checkin
+
+          if phase == 'On' and current_phase == 'off':
+              if off_start_time_prod is not None:
+                  off_time_prod += checkin - off_start_time_prod
+                  off_start_time_prod = None
+
+          if pd.notnull(phase):
+                  current_phase = phase
+
+
+          # Check if this row belongs to the same job and if it's a later timestamp
+          if job == current_job and checkin > production_start_time:
+              job_production_times[current_job] = checkin - production_start_time
+              job_non_productive_times[current_job] = non_productive_time
+              job_basic_make_ready_times[current_job] = basic_make_ready_time
+              job_fine_tune_times[current_job] = fine_tune_time
+              job_off_times_pre[current_job] = off_time_pre
+              job_off_times_prod[current_job] = off_time_prod
+
+      # Calculate the job time for each job
+      df['JobTime'] = df.groupby('Job')['CheckOut'].transform('max') - df.groupby('Job')['CheckIn'].transform('min')
+      job_job_times = df.groupby('Job')['JobTime'].first()
+
+      # Create a list to store the KPI for each job
+      As = []
+      Ps = []
+      Qs = []
+      OEEs = []
+
+      # Print the production time, job time, non-productive time, quotient, gained net, and kpi for each job
+      for job in job_production_times.keys():
+          production_time = job_production_times.get(job, pd.Timedelta(0))
+          non_productive_time = job_non_productive_times.get(job, pd.Timedelta(0))
+          basic_make_ready_time = job_basic_make_ready_times.get(job, pd.Timedelta(0))
+          fine_tune_time = job_fine_tune_times.get(job, pd.Timedelta(0))
+          job_time = job_job_times.get(job, pd.Timedelta(0))
+          gained_net = job_gained_net.get(job, 0)
+          gained_gross = job_gained_gross.get(job, 0)
+          off_time_pre = job_off_times_pre.get(job, 0)
+          off_time_prod = job_off_times_prod.get(job, 0)
+          Max_speed = job_max_speed.get(job, 0)
+
+          A = (production_time.total_seconds() - non_productive_time.total_seconds() - off_time_prod.total_seconds())/ (production_time.total_seconds() + fine_tune_time.total_seconds() + basic_make_ready_time.total_seconds() - off_time_prod.total_seconds())
+          P = ((3600/Max_speed) *gained_gross) / (production_time.total_seconds() - non_productive_time.total_seconds() - off_time_prod.total_seconds())
+          Q = gained_net / gained_gross
+          OEE = A * P * Q
+
+          As.append(A)
+          Ps.append(P)
+          Qs.append(Q)
+          OEEs.append(OEE)
+
+          print(f'Job {job}: A = {A}, P = {P}, Q= {Q}, OEE = {OEE}')
+
+      # Create a line chart for KPI
+      # KPI_figure = plt.figure(figsize=(10, 6))
+      # plt.plot(range(1, len(OEEs) + 1), OEEs, color='green', label='OEE', marker='o')
+      # plt.xlabel('Job')
+      # plt.ylabel('OEE')
+      # plt.title('OEE for each Job - Machine A')
+      # plt.xticks(range(1, len(OEEs) + 1))
+      # plt.grid(True)
+      # plt.show()
+
+     # Assuming OEEs is your data
+      x_values = list(range(1, len(OEEs) + 1))
+
+      KPI_figure = go.Figure()
+      KPI_figure.add_trace(go.Scatter(x=x_values, y=OEEs, mode='lines+markers', name='OEE', marker=dict(color='green')))
+      KPI_figure.update_layout(title='OEE for each Job - Machine A',
+                        xaxis_title='Job',
+                        yaxis_title='OEE',
+                        xaxis=dict(tickvals=x_values, tickmode='linear'),
+                        yaxis=dict(gridcolor='lightgrey'),
+                        plot_bgcolor='white')
+
+      st.plotly_chart(KPI_figure)
+
+      #Maschine A - Prediction
+
+      # Read the CSV file into a pandas DataFrame
+      #df = pd.read_csv(uploaded_cluster_file_KPI, sep=';')
+      #st.write(df.head())
+
+      new_speed_value = st.number_input("Enter New Speed Value:", value=new_speed_value)
+
+
+      def lin_reg():
+        single_variable = OEEs
+        average_speed_per_job = df.groupby('Job')['Speed'].mean()
+
+        # Replace these with your actual data
+        single_variable = np.array(single_variable)
+        average_speed_per_job = np.array(average_speed_per_job)
+
+        # Add a constant term for the intercept
+        X = sm.add_constant(average_speed_per_job)
+
+        # Fit the linear regression model with average_speed_per_job as the independent variable
+        model = sm.OLS(single_variable, X).fit()
+
+        # Get the slope and intercept from the regression model
+        slope = model.params[1]
+        intercept = model.params[0]
+
+        # Calculate the prediction for a new value of average_speed_per_job
+        # Create an input box for the user to input the value of new_speed_value
+        #new_speed_value = st.number_input("Enter New Speed Value:", value=new_speed_value)
+        predicted_value = slope * new_speed_value + intercept
+
+        # Get the standard error of the prediction
+        se = np.sqrt(model.scale)  # Standard error of the residuals
+
+        # Calculate the confidence interval for the prediction
+        alpha = 0.05  # Significance level
+        t_value = t.ppf(1 - alpha / 2, df=model.df_resid)  # T-distribution critical value
+        margin_of_error = t_value * se
+        prediction_ci = (predicted_value - margin_of_error, predicted_value + margin_of_error)
+
+        # Create a scatter plot
+        fig, ax = plt.subplots()
+        ax.scatter(average_speed_per_job, single_variable, label='Data points')
+
+        # Plot the regression line
+        regression_line = model.predict(X)
+        ax.plot(average_speed_per_job, regression_line, color='red', label='Regression Line')
+
+        # Highlight the new value and its prediction with the correct confidence interval
+        ax.scatter(new_speed_value, predicted_value, color='green', label='Predicted Value')
+        ax.plot([new_speed_value, new_speed_value], [prediction_ci[0], prediction_ci[1]], color='green', linestyle='--', label='95% CI')
+
+        # Add labels and a title
+        ax.set_xlabel('Average Speed per Job')
+        ax.set_ylabel('OEE')
+        ax.set_title('Scatter Plot with Regression Line and Prediction Interval')
+
+        # Show the plot
+        ax.legend()
+        st.pyplot(fig)
+
+        st.write(f"The confidence interval of 95% lies between {prediction_ci[0]} and {prediction_ci[1]}.")
+
+      if st.button("See Linear Regression"):
+        # When the button is clicked, call your function
+        lin_reg()
 
 def page2():
     
