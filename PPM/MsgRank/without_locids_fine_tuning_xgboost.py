b804ed8284d36fa8b22c875155372d452f2ac9f3
annkathrinlei <158102865+annkathrinlei@users.noreply.github.com>
2024-05-17 10:12:47 +0200

    Update without_locids_fine_tuning_xgboost.py

--- PPM/MsgRank/without_locids_fine_tuning_xgboost.py
+++ PPM/MsgRank/without_locids_fine_tuning_xgboost.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/11Gt5TUsvyCuWBr5A-Va8fLzv_4W71hi7
 """
 
-import tensorflow as tf
 import os
 import pandas as pd
 import numpy as np
