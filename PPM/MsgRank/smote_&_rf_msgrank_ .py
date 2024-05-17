dd6a984c032d3e20e6309f66f3f644185789e421
annkathrinlei <158102865+annkathrinlei@users.noreply.github.com>
2024-05-17 10:12:11 +0200

    Update smote_&_rf_msgrank_ .py

--- PPM/MsgRank/smote_&_rf_msgrank_ .py
+++ PPM/MsgRank/smote_&_rf_msgrank_ .py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/11zmEpVhXfy-QXfhH71bJBRTLtDAEtN1h
 """
 
-import tensorflow as tf
 import os
 import pandas as pd
 import numpy as np
